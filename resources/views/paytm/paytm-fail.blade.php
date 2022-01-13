b7f4f21210ed32d5c891d784ac6cf13cee1ddc9b
adanelahi <adan.vayuz@gmail.com>
2022-01-13 19:15:28 +0530

    first commit

---
+++ resources/views/paytm/paytm-fail.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="alert alert-success alert-dismissible fade" role="alert">
+    <strong>Your Payment Has been failed</strong>
+</div>
+<a href="{{route('paytm.purchase')}}">Check the demo again</a>
+</body>
+</html>
